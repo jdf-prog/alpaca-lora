c728e4b990518ee204b8b0958273141ea8beedb5
dongfu <dongfu.jdf@gmail.com>
2023-08-11 01:48:14 -0700

    update le

--- finetune.sh
+++ finetune.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=./jobs/%j.out
 
-run_name="new_data_full_lora_layer_l1536_r8_alpha16_dropout0.05_lr2e-5"
+run_name="new_data_full_lora_layer_l1536_r8_alpha16_dropout0.05_lr3e-4"
 base_model="meta-llama/Llama-2-13b-hf"
 output_dir="./lora-alpaca/${base_model}/${run_name}"
 # data_path="/home/dongfu/WorkSpace/ExplainableGPTScore/finetune_data/translation/train.json" # old data
@@ -17,7 +17,7 @@ python finetune.py \
     --batch_size 128 \
     --micro_batch_size 4 \
     --num_epochs 3 \
-    --learning_rate 2e-5 \
+    --learning_rate 3e-4 \
     --cutoff_len 1536 \
     --val_set_size 500 \
     --lora_r 8 \
